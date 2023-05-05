25da12811c31121f69663fcdadf05a4086bbaa88
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-05 01:43:51 +0000

    added code for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function myButtonClicked() {
+  // This function tells he user if they get free admission
+  // input
+  const userAge = parseInt(document.getElementById("user-age").value)
+  const dayOfTheWeek = document.getElementById("day-of-the-week").value
+
+  // process
+  if ((dayOfTheWeek == "Monday" || dayOfTheWeek == "Thursday") || (userAge > 12 && userAge < 21)) {
+    // output
+    document.getElementById("answer").innerHTML = "You get free admission!"
+  } else {
+    // output
+    document.getElementById("answer").innerHTML = "You have to pay admission."
+  }
+}
